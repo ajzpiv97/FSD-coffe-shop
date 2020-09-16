5e8f74e4e86c003d8163ebe1a1243005e1ef4a26
Armando Zubillaga <ajzpiv97@gmail.com>
2020-09-16 12:46:08 -0400

    Made changes

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'dev-hevrtq9y.us.auth0.com', // the auth0 domain prefix
+    url: 'dev-hevrtq9y.us', // the auth0 domain prefix
     audience: 'coffeeShop', // the audience set for the auth0 app
     clientId: 'atQzmYNiCKm5mALv1IG86HP2nJgXIh1J', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
   }
 };
